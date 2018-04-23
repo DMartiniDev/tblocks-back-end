f97aa7763e6a06bc69f9e0a00783b5f8b4d257a1
Dave Martinez <dkm.coder@gmail.com>
2018-04-23 15:39:43 +0200

    Chore: Send player data when pairing opponents

--- sockets_logic.js
+++ sockets_logic.js
@@ -50,13 +50,13 @@ exports.makePlayerAvailable = (socket, name) => {
   if (availablePlayers.length === 0) {
     availablePlayers.push(newPlayer);
     // Now that the user has been made available, update the client
-    socket.nsp.to(socket.id).emit('updateClient', {status:'wait'});
+    socket.nsp.to(socket.id).emit('updateClient', {status:'wait', player: null, opponent: null});
   } else {
     let opponent = availablePlayers.splice(0, 1)[0];
     console.log('Oponent:', opponent);
 
-    socket.nsp.to(socket.id).emit('updateClient', {status:'pair'});
-    socket.nsp.to(opponent.id).emit('updateClient', {status:'pair'});
+    socket.nsp.to(socket.id).emit('updateClient', {status:'pair', player: newPlayer, opponent: opponent});
+    socket.nsp.to(opponent.id).emit('updateClient', {status:'pair', player: opponent, opponent: newPlayer});
     console.log(`Pairing ${newPlayer.name} with ${opponent.name}`);
 
   }
