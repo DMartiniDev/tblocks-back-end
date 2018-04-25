a2b20ede7545c1c3922e01c90764e38a63a2a312
Dave Martinez <dkm.coder@gmail.com>
2018-04-25 03:23:09 +0200

    Feat: Add game logic to the backend

--- sockets_logic.js
+++ sockets_logic.js
@@ -8,30 +8,21 @@ module.exports.availablePlayers = availablePlayers;
 module.exports.allPlayers = allPlayers;
 
 exports.disconnect = (socket) => {
-  console.log('Disconnected:', socket.id);
-
   // If the user which disconnected was in the list of available players,
   // remove it from there
   let indexToDelete = null;
   for (let i = 0; i < availablePlayers.length; i++) {
     const currentPlayer = availablePlayers[i];
-    console.log(`Comparting ${currentPlayer.id} with ${socket.id}`);
 
     if (currentPlayer.id === socket.id) {
-      console.log('MATCH - An element will be deleted');
 
       indexToDelete = String(i);
     }
   }
 
-  console.log("The index to delete is:", indexToDelete);
-
   if (indexToDelete) {
     availablePlayers.splice(Number(indexToDelete), 1);
   }
-
-  console.log('Available Players:');
-  console.log(JSON.stringify(availablePlayers, null, 4));
 
   allPlayers.splice(allPlayers.indexOf(socket.id), 1);
   playerCount = allPlayers.length;
@@ -56,24 +47,326 @@ exports.makePlayerAvailable = (socket, name) => {
     socket.nsp.to(socket.id).emit('updateClient', {status:'wait', player: null, opponent: null});
   } else {
     let opponent = availablePlayers.splice(0, 1)[0];
-    console.log('Oponent:', opponent);
-
-    console.log(`Pairing ${newPlayer.name} with ${opponent.name}`);
 
     // Create Game in the server:
     const gameID = uuid();
-    console.log('Created a new game with the ID:', gameID);
-    games[gameID] = {players: [newPlayer.id, opponent.id]};
-    console.log('List of games:');
-    console.log(JSON.stringify(games, null, 4));
+
+    // Create a match
+    const p1Board = new TeltrisGame(gameID, newPlayer.id, socket);
+    const p2Board = new TeltrisGame(gameID, opponent.id, socket);
+
+    games[gameID] = {
+      players: [newPlayer.id, opponent.id],
+      boards: [p1Board, p2Board]
+    };
 
     newPlayer.gameID = gameID;
     opponent.gameID = gameID;
 
     socket.nsp.to(socket.id).emit('updateClient', {status:'pair', player: newPlayer, opponent: opponent});
     socket.nsp.to(opponent.id).emit('updateClient', {status:'pair', player: opponent, opponent: newPlayer});
+    p1Board.playerReset();
+    p1Board.update();
+    p2Board.playerReset();
+    p2Board.update();
   }
-  console.log('Available Players:');
-  console.log(JSON.stringify(availablePlayers, null, 4));
+}
 
+
+
+
+colors = [
+  null,
+  '#a000f0',    // T
+  '#f0a000',    // L
+  '#0000f0',    // J
+  '#00f000',    // S
+  '#f00000',    // Z
+  '#00f0f0',    // L
+  '#f0f000'     // O
+];
+
+class TeltrisGame {
+  constructor(gameID, playerID, socket) {
+    this.socket = socket;
+    this.gameID = gameID;
+    this.playerID = playerID;
+    // ------ Game Data ------
+    // Board
+    this.arena = createMatrix(12, 20);
+
+    // Piece
+    this.player = {
+      pos: {
+        x: 4,
+        y: 0
+      },
+      matrix: null,
+      score: 0,
+    }
+
+    this.dropCounter = 0;
+    this.dropInterval = 85;
+    this.lastTime = 0;
+
+    // ------ Initialisation ------
+    // Initialisation of the game
+    // TODO: this.updateScore();
+  }
+
+  testGame() {
+    setInterval(() => {
+      if (this.arena[2][0] === 1) {
+        this.arena[2].fill(0);
+      } else {
+        this.arena[2].fill(1);
+      }
+
+      for (const currentClient of games[this.gameID].players) {
+        this.socket.nsp.to(currentClient).emit('updateBoard', {board:this.arena, playerID: this.playerID});
+      }
+    }, 1000);
+  }
+
+
+
+
+
+
+
+  // ------ Functions ------
+  clear() {
+    arenaSweep(this.arena, this.player);
+  }
+
+  playerDrop() {
+    this.player.pos.y++;
+    if (collide(this.arena, this.player)) {
+      this.player.pos.y--;
+      merge(this.arena, this.player);
+      this.playerReset();
+      arenaSweep(this.arena, this.player);
+    }
+    this.dropCounter = 0;
+  }
+
+  playerDropToBottom() {
+    while (!collide(this.arena, this.player)) {
+      this.player.pos.y++;
+    }
+
+    this.player.pos.y--;
+    merge(this.arena, this.player);
+    this.playerReset();
+    arenaSweep(this.arena, this.player);
+    this.dropCounter = 0;
+  }
+
+  playerReset() {
+    const pieces = 'ILJOTSZ';
+    this.player.matrix = createPiece(pieces[Math.floor(pieces.length * Math.random())]);
+    this.player.pos.y = 0;
+    this.player.pos.x = (this.arena[0].length / 2 | 0) - (Math.floor(this.player.matrix[0].length / 2));
+
+    if (collide(this.arena, this.player)) {
+      // Get the name of the player who lost
+      let loser;
+      for (let z = 0; z < availablePlayers.length; z++) {
+        const currentPlayer = availablePlayers[z];
+        if (currentPlayer.id === this.playerID) {
+          loser = currentPlayer.name;
+        }
+      }
+
+      for (const currentClient of games[this.gameID].players) {
+        this.socket.nsp.to(currentClient).emit('finishGame', {loser: loser});
+      }
+      // this.player.score = 0;
+      // this.updateScore();
+    }
+  }
+
+  update() {
+    setInterval(() => {
+      this.playerDrop();
+      for (const currentClient of games[this.gameID].players) {
+        this.socket.nsp.to(currentClient).emit('updateBoard', {board: this.arena, player:this.player, playerID: this.playerID});
+      }
+    }, this.dropInterval)
+  }
+}
+
+
+// ----------------------- GAME LOGIC ------------------------------------- //
+/**
+ * Clear any lines that can be cleared in the arena specified. For each line
+ * cleared, add 10 points to the score of the player.
+ */
+arenaSweep = (arena, player) => {
+  let rowCount = 1;
+  outer: for (let y = arena.length -1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+
+    player.score += rowCount*10;
+  }
+}
+
+playerMove = (direction, player, arena) => {
+  player.pos.x += direction;
+
+  if(collide(arena, player)) {
+    player.pos.x -= direction;
+  }
+}
+
+playerRotate = (dir, player, arena) => {
+  rotate(player.matrix, dir);
+  const pos = player.pos.x;
+  let offset = 1;
+
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1: -1));
+
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+rotate = (matrix, direction) => {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < y; x++) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
+
+  if (direction > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
+/**
+ * Check if there is a collision between the player's piece and
+ */
+collide = (arena, player) => {
+  const [m, o] = [player.matrix, player.pos];
+
+  for (let y=0; y < m.length; y++) {
+    for (let x=0;x< m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y+o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+}
+
+createPiece = (type) => {
+  if (type === 'T') {
+    return [
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+  }
+
+  if (type === 'L') {
+    return [
+      [0,2,0],
+      [0,2,0],
+      [0,2,2]
+    ]
+  }
+
+  if (type === 'J') {
+    return [
+      [0,3,0],
+      [0,3,0],
+      [3,3,0]
+    ]
+  }
+
+  // z
+  if (type === 'Z') {
+    return [
+      [0,4,4],
+      [4,4,0],
+      [0,0,0]
+    ]
+  }
+
+  // s
+  if (type === 'S') {
+    return [
+      [5,5,0],
+      [0,5,5],
+      [0,0,0]
+    ]
+  }
+
+  if (type === 'I') {
+    return [
+      [0, 0, 6, 0, 0],
+      [0, 0, 6, 0, 0],
+      [0, 0, 6, 0, 0],
+      [0, 0, 6, 0, 0],
+      [0, 0, 6, 0, 0]
+    ]
+  }
+
+  if (type === 'O') {
+    return [
+      [7,7],
+      [7,7]
+    ]
+  }
+}
+
+// ------------------------------- Helper Functions ------------------------- //
+boardToDraw = (arena, player) => {
+  const newBoard = arena.slice(0);
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0) {
+        newBoard[y + player.pos.y][x + player.pos.x] = value;
+      }
+    })
+  })
+  return newBoard;
+}
+
+createMatrix = (width, height) => {
+  const matrix = [];
+  while (height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+}
+
+merge = (arena, player) => {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    })
+  })
 }
